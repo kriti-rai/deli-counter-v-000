f5ad5d1df03b689f7643d31d83995dde9cf14946
Kriti Rai <kriti.rai@gmail.com>
2018-01-05 03:10:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  # shows everyone in the line and their position.
+  #puts "The line is currently empty" if empty
+end
+
+def take_a_number(katz_deli, name)
+  #Returns the person's name along with their position in line
+end
+
