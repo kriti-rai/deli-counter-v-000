d5f9f257633f165c4a60886df11f7ab068f4d684
Kriti Rai <kriti.rai@gmail.com>
2018-01-05 03:11:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,8 @@ def take_a_number(katz_deli, name)
   #Returns the person's name along with their position in line
 end
 
+def now_serving
+  #puts the next person in line
+  #removes them from the front
+  #if there is nobody in line, puts "There is nobody waiting to be served!"
+end
