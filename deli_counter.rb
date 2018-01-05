b08a0ed89983cccb7fca55227fd9e75a46783ee4
Kriti Rai <kriti.rai@gmail.com>
2018-01-05 04:35:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   	current_line = []
   	katz_deli.each_with_index {|a,i|
   	current_line << "#{i+1}. #{a}"}
-  	puts "The line currently is: " + current_line.join(" ")
+  	puts "The line is currently: " + current_line.join(" ")
   end
 end
 
